3f73e0748aae7e5daf15e7789dfc549667ca3890
palomishah21 <palomi.shah21@gmail.com>
2021-07-15 17:34:51 +0530

    Initial commit

---
+++ sum.java
@@ -0,0 +1,34 @@
+
+user@DESKTOP-OL54O8F MINGW64 ~
+public class Sum{
+	public static void main(String[] args){
+		int num1=10,num2=20,sum
+			sum=num1+num2
+			System.out.println("sum is: "+sum)
+	}
+}
+
+wq!
+
+$ mkdir Learngit
+
+user@DESKTOP-OL54O8F MINGW64 ~
+$ cd Learngit/
+
+user@DESKTOP-OL54O8F MINGW64 ~/Learngit
+$ pwd
+/c/Users/user/Learngit
+
+user@DESKTOP-OL54O8F MINGW64 ~/Learngit
+$ ls
+
+user@DESKTOP-OL54O8F MINGW64 ~/Learngit
+$ git init
+Initialized empty Git repository in C:/Users/user/Learngit/.git/
+
+user@DESKTOP-OL54O8F MINGW64 ~/Learngit (master)
+$ ls -a
+./  ../  .git/
+
+user@DESKTOP-OL54O8F MINGW64 ~/Learngit (master)
+$
