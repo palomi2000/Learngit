d98e6ada561b9e59c3333e1b2ed703f3753c0961
palomishah21 <palomi.shah21@gmail.com>
2021-07-15 17:55:57 +0530

    modified sum.java

--- sum.java
+++ sum.java
@@ -1,34 +1,7 @@
-
-user@DESKTOP-OL54O8F MINGW64 ~
 public class Sum{
 	public static void main(String[] args){
 		int num1=10,num2=20,sum
 			sum=num1+num2
-			System.out.println("sum is: "+sum)
+			System.out.println("sum of numbers is: "+sum)
 	}
 }
-
-wq!
-
-$ mkdir Learngit
-
-user@DESKTOP-OL54O8F MINGW64 ~
-$ cd Learngit/
-
-user@DESKTOP-OL54O8F MINGW64 ~/Learngit
-$ pwd
-/c/Users/user/Learngit
-
-user@DESKTOP-OL54O8F MINGW64 ~/Learngit
-$ ls
-
-user@DESKTOP-OL54O8F MINGW64 ~/Learngit
-$ git init
-Initialized empty Git repository in C:/Users/user/Learngit/.git/
-
-user@DESKTOP-OL54O8F MINGW64 ~/Learngit (master)
-$ ls -a
-./  ../  .git/
-
-user@DESKTOP-OL54O8F MINGW64 ~/Learngit (master)
-$
